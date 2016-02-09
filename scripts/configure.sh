98611c3f7f711831982b0a27044132f0fe863b95
Taranjeet <reachtotj@gmail.com>
2016-02-09 21:59:16 +0530

    Add configure script

---
+++ scripts/configure.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# cd ..
+
+# echo 'Creating virtual env'
+# virtualenv --no-site-packages pyenv
+
+# echo 'Activating virtual env'
+# source pyenv/bin/activate
+
+# echo 'Installing requirements'
+# pip install -r requriements/dev.txt
+
+# before running bower install,
+# make sure {{project_name}}/static/bower_components exists
+
+# if [ ! -d "{{project_name}}/static/bower_components" ]; then
+#     mkdir -p "{{project_name}}/static/bower_components"
+# fi
+
+# if [ -f 'bower.json' ]; then
+    # echo 'Running bower install'
+    # bower install
+# fi
+
+# before running collectstatic, make sure that
+# `root_dir/static` exists
+
+if [ ! -d "static" ]; then
+    mkdir "static"
+fi
+
+# echo 'Collecting static files'
+# python manage.py collectstatic --noinput
+
+# echo 'Running migrations'
+# python manage.py migrate --noinput
