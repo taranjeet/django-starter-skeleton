f81e60f0c96bb3d13c04269860a2533a71c1fbff
Taranjeet <reachtotj@gmail.com>
2016-02-09 22:20:09 +0530

    configure.sh: Update bowerrc and its creation

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -2,26 +2,35 @@
 
 # cd ..
 
-# echo 'Creating virtual env'
-# virtualenv --no-site-packages pyenv
+echo "Creating virtual env"
+virtualenv --no-site-packages pyenv
 
-# echo 'Activating virtual env'
-# source pyenv/bin/activate
+echo "Activating virtual env"
+source pyenv/bin/activate
 
-# echo 'Installing requirements'
-# pip install -r requriements/dev.txt
+echo "Installing requirements"
+pip install -r requriements/dev.txt
 
 # before running bower install,
 # make sure {{project_name}}/static/bower_components exists
 
-# if [ ! -d "{{project_name}}/static/bower_components" ]; then
-#     mkdir -p "{{project_name}}/static/bower_components"
-# fi
+if [ ! -d "{{project_name}}/static/bower_components" ]; then
+    mkdir -p "{{project_name}}/static/bower_components"
+fi
 
-# if [ -f 'bower.json' ]; then
-    # echo 'Running bower install'
-    # bower install
-# fi
+# create `.bowerrc` dynamically, so that template variable
+# project_name is substituted properly
+
+cat <<'EOF' > ".bowerrc"
+{
+    "directory": "{{project_name}}/static/bower_components"
+}
+EOF
+
+if [ -f "bower.json" ]; then
+    echo "Running bower install"
+    bower install
+fi
 
 # before running collectstatic, make sure that
 # `root_dir/static` exists
@@ -30,8 +39,8 @@ if [ ! -d "static" ]; then
     mkdir "static"
 fi
 
-# echo 'Collecting static files'
-# python manage.py collectstatic --noinput
+echo "Collecting static files"
+python manage.py collectstatic --noinput
 
-# echo 'Running migrations'
-# python manage.py migrate --noinput
+echo "Running migrations"
+python manage.py migrate --noinput
