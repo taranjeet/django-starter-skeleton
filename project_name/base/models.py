bd3e779892133f132f2c4d98a2d047640591557d
Taranjeet <reachtotj@gmail.com>
2016-02-11 08:55:02 +0530

    base app: Add audit model

---
+++ project_name/base/models.py
@@ -0,0 +1,30 @@
+from django.contrib
+from django.db import models
+
+class TimeAuditModel(models.Model):
+    ''' To track when the record was created and last modified'''
+    created_at = models.DateTimeField('Created At',
+        auto_now_add=True)
+    updated_at = models.DateTimeField('Updated At',
+        auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class UserAuditModel(models.Model):
+    '''To track who created and last modified the record'''
+    created_by = models.ForeignKey(User, related_name='created_%(class)s_set',
+                                   null=True, blank=True, verbose_name='Created By')
+    updated_by = models.ForeignKey(User, related_name='updated_%(class)s_set',
+                                  null=True, blank=True, verbose_name='Updated By')
+
+    class Meta:
+        abstract = True
+
+
+class AuditModel(TimeAuditModel, UserAuditModel):
+    '''To track by who and when was the last record modified'''
+
+    class Meta:
+        abstract = True
