eb0664af3af8d200a92ec63a0f479d4aec6821df
Taranjeet Singh <reachtotj@gmail.com>
2018-09-18 17:28:17 +0530

    Read config variable from environment.

--- src/settings/dev.py
+++ src/settings/dev.py
@@ -4,10 +4,3 @@ import os
 from .common import *       # noqa
 
 DEBUG = True
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(ROOT_DIR, 'db.sqlite3'),       # noqa
-    }
-}
