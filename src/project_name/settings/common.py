3d32c13d90bcb05d97d2d35a815c6dfa4a99aad6
Taranjeet Singh <reachtotj@gmail.com>
2018-09-26 01:05:29 +0530

    Add default for secret key.

--- src/project_name/settings/common.py
+++ src/project_name/settings/common.py
@@ -25,7 +25,7 @@ if exists(env_file):
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY', default='new-secret-key-here')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
