a5e7b192727d66a0418080f3e1e9f24faac8efd8
Taranjeet Singh <reachtotj@gmail.com>
2018-10-08 13:30:33 +0530

    Fix urls.

--- src/project_name/urls.py
+++ src/project_name/urls.py
@@ -1,7 +1,7 @@
-from django.urls import include, path
+from django.urls import path
 from django.contrib import admin
 
 urlpatterns = [
 
-    path('^admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
 ]
