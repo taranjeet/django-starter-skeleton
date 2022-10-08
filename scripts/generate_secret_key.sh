ef79d465765f32155344bd1d355b7e060e44c38a
Taranjeet Singh <reachtotj@gmail.com>
2022-10-08 20:46:48 +0530

    Fresh start for django 4.0+

--- scripts/generate_secret_key.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python -c 'import random; import string; print("".join([random.SystemRandom().choice(string.digits + string.ascii_letters + string.punctuation) for i in range(100)]))'
